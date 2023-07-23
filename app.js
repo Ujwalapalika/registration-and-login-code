1808cb67068f80e52fef731345e1b17b32633fe9
ujwala palika <ujwalapalika@gmail.com>
2023-07-23 05:29:58 +0000

    solution

--- app.js
+++ app.js
@@ -4,8 +4,24 @@ const sqlite3 = require("sqlite3");
 const path = require("path");
 const bcrypt = require("bcrypt");
 const app = express();
-const db = path.join(__dirname, "userData.db");
+const dbase = path.join(__dirname, "userData.db");
 app.use(express.json());
+let db = null;
+const intializedbserver = async () => {
+  try {
+    db = await open({
+      filename: dbase,
+      driver: sqlite3.Database,
+    });
+    app.use(3000, () => {
+      console.log("Server is running");
+    });
+  } catch (e) {
+    console.log(`Db error :${e.message}`);
+    process.exit(1);
+  }
+};
+intializedbserver();
 app.post("/register", async (request, response) => {
   const { username, name, password, gender, location } = request.body;
   const hashedpswd = await bcrypt.hash(request.body.password, 10);
@@ -13,12 +29,11 @@ app.post("/register", async (request, response) => {
   const dbuser = await db.get(getuser);
   if (dbuser === undefined) {
     const createuser = `insert into user(username,name,password,gender,location) values("${username}","${name}","${hashedpswd}","${gender}","${location}")`;
-    const dbresponse = await db.run(createuser);
-    const userId = dbresponse.lastId;
-    if (password.length() <= 5) {
+    if (password.length() < 5) {
       response.status = 400;
       response.send("Password is too short");
     } else {
+      const dbresponse = await db.run(createuser);
       response.status = 200;
       response.send("User created successfully");
     }
